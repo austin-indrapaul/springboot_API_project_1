fe4b74fbbb9302cd57e40175c2b9bc0d665f6500
Austin <austin.indrapaul@gmail.com>
2022-11-03 18:22:35 +0530

    New Project Created

---
+++ src/main/java/demo/springboot_API_project_1/SpringbootApiProject1Application.java
@@ -0,0 +1,13 @@
+package demo.springboot_API_project_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/demo/springboot_API_project_1/SpringbootApiProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springboot_API_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
